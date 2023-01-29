22b132bfaac4048beab7d42900395d3545761b09
Nicholas Galante <nicholasgalante94@gmail.com>
2023-01-28 19:45:15 -0500

    Add formData state and handleChange on entry form

--- src/Components/EntryEditor.js
+++ src/Components/EntryEditor.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
-function EntryEditor(){
+function EntryEditor() {
+   const [formData, setFormData] = useState({
+      title : "",
+      content : "",
+      date : "",
+   })
+
+   function handleChange(event){
+      event.preventDefault();
+      setFormData({
+         ...formData,
+         [event.target.name]: event.target.value,
+       });
+      console.log(formData)
+   }
+
+   function handleSubmit(){
+      
+   }
+
    return (
       <div>
          EntryEditor
+         <form id="entryEditor">
+            <input
+               type="text"
+               name="title"
+               onChange={handleChange}
+            />
+            <textarea id="entryEditor"
+               name="content"
+               rows="15"
+               cols="100"
+               onChange={handleChange}
+               placeholder="Your text here ">
+            </textarea>
+            <button type="submit" onClick={handleSubmit}>Save Entry</button>
+         </form>
       </div>
    )
 }
